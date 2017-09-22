c860b259fd45942b1dc48e1fe232bf85e59dea32
Sebastian Boberg <sebastianboberg@gmail.com>
2017-09-22 13:58:08 -0700

    api/image endpoint for image uploads

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var controllers = require('./controllers/controllers.js');
 
 router.post('/addItem', controllers.addItem.post);
+router.post('/api/image', controllers.uploadImage.post);
 router.get('/testConnection', controllers.testConnection.get);
 
 module.exports = router;
