03c733eded9b62851d2afd9deb1326a285717e49
Sebastian Boberg <sebastianboberg@gmail.com>
2017-09-22 13:58:21 -0700

    Images upload to S3

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,10 @@
 var dbHandlers = require('../db/dbHandlers.js');
 var index = require('../algolia/algoliaConnection.js');
+var multiparty = require('multiparty');
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+AWS.config.update({ accessKeyId: process.env.AWS_ID, secretAccessKey: process.env.AWS_KEY });
 
 exports.addItem = {
   post: (req, res) => {
@@ -27,5 +32,37 @@ exports.addItem = {
 exports.testConnection = {
   get: (req, res) => {
     res.sendStatus(200);
+  }
+}
+
+exports.uploadImage = {
+  post: (req, res) => {
+    var form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+
+      console.log(fields);
+      console.log(files);
+
+      const s3 = new AWS.S3({region: 'us-west-1'});
+      var base64data = new Buffer(files.photo, 'binary');
+
+      var params = {
+        Bucket: 'wasteline-images', 
+        Key: fields.product[0], 
+        ACL: 'public-read',
+        Body: fs.createReadStream(files.photo[0].path)
+      }
+
+      s3.putObject(params, function (err, data) {
+        if (err) {
+            console.log("Error uploading image: ", err);
+          } else {
+            console.log("Successfully uploaded image on S3", data);
+          }
+      });
+    
+      console.log('Upload completed!');
+      res.sendStatus(200);
+    });
   }
 }
